8181d638bdb754a63a83838a05a5444979a91694
Maggieykw <30870497+Maggieykw@users.noreply.github.com>
2021-08-29 13:57:55 +0800

    Update MovieLens.R

--- MovieLens.R
+++ MovieLens.R
@@ -54,9 +54,6 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 # 1.Create a movie recommendation system using the MovieLens dataset.
 ##########################################################
 
-train_set <- edx
-test_set <- validation
-
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
@@ -65,11 +62,11 @@ RMSE <- function(true_ratings, predicted_ratings){
 # 1a.Consider average in the model only
 ##########################################################
 
-mu_hat <- mean(train_set$rating)
-naive_rmse <- RMSE(test_set$rating, mu_hat)
+mu_hat <- mean(edx$rating)
+naive_rmse <- RMSE(validation$rating, mu_hat)
 #naive_rmse #for result testing
 
-#rmse_results <- data_frame(method = "Just the average", RMSE = naive_rmse) #for result testing
+rmse_results <- data_frame("Predictive Method" = "Just the average", RMSE = naive_rmse)
 #rmse_results #for result testing
 #RMSE of including average only: 1.0612018
 
@@ -77,20 +74,20 @@ naive_rmse <- RMSE(test_set$rating, mu_hat)
 # 1b.Add movie effect into model
 ##########################################################
 
-movie_avgs <- train_set %>% 
+movie_avgs <- edx %>% 
   group_by(movieId) %>% 
   summarize(b_i = mean(rating - mu_hat))
 
-predicted_ratings <- mu_hat + test_set %>% 
+predicted_ratings <- mu_hat + validation %>% 
   left_join(movie_avgs, by='movieId') %>%
   .$b_i
 
-model_1_rmse <- RMSE(predicted_ratings, test_set$rating)
+model_1_rmse <- RMSE(predicted_ratings, validation$rating)
 #model_1_rmse #for result testing
 
-#rmse_results <- bind_rows(rmse_results, #for result testing
-                          #data_frame(method="Movie Effect Model",
-                                     #RMSE = model_1_rmse ))
+rmse_results <- bind_rows(rmse_results,
+                          data_frame("Predictive Method"="Movie Effect Model",
+                                     RMSE = model_1_rmse ))
 #rmse_results #for result testing
 #RMSE of including average and movie effect model: 0.9439087
 
@@ -98,30 +95,138 @@ model_1_rmse <- RMSE(predicted_ratings, test_set$rating)
 # 1c.Add user-specifc effect into model
 ##########################################################
 
-user_avgs <- test_set %>% 
+user_avgs <- edx %>% 
   left_join(movie_avgs, by='movieId') %>%
   group_by(userId) %>%
   summarize(b_u = mean(rating - mu_hat - b_i))
 
-predicted_ratings <- test_set %>% 
+predicted_ratings <- validation %>% 
   left_join(movie_avgs, by='movieId') %>%
   left_join(user_avgs, by='userId') %>%
   mutate(pred = mu_hat + b_i + b_u) %>%
   .$pred
 
-model_2_rmse <- RMSE(predicted_ratings, test_set$rating)
+model_2_rmse <- RMSE(predicted_ratings, validation$rating)
 #model_2_rmse #for result testing
 
-#rmse_results <- bind_rows(rmse_results, #for result testing
-                          #data_frame(method="Movie + User Effects Model",  
-                                     #RMSE = model_2_rmse ))
+rmse_results <- bind_rows(rmse_results,
+                          data_frame("Predictive Method"="Movie + User Effects Model",  
+                                     RMSE = model_2_rmse ))
 #rmse_results %>% knitr::kable() #for result testing
-#RMSE of including average, movie effect model and user effect model: 0.8292477 (which is the final RMSE result as well)
+#RMSE of including average, movie effect model and user effect model: 0.8653488
+
+##########################################################
+# 1d.Regularization for movie-specifc effect in the model
+##########################################################
+
+#Choosing the penalty terms using cross-validation
+lambdas <- seq(0, 10, 0.25)
+
+just_the_sum <- edx %>% 
+  group_by(movieId) %>% 
+  summarize(s = sum(rating - mu_hat), n_i = n())
+
+rmses <- sapply(lambdas, function(l){
+  predicted_ratings <- validation %>% 
+    left_join(just_the_sum, by='movieId') %>% 
+    mutate(b_i = s/(n_i+l)) %>%
+    mutate(pred = mu_hat + b_i) %>%
+    pull(pred)
+  return(RMSE(predicted_ratings, validation$rating))
+})
+
+#qplot(lambdas, rmses)  
+#lambdas[which.min(rmses)]
+#lambda = 2.5 based on the above cross-validation 
+
+lambda <- lambdas[which.min(rmses)]
+movie_reg_avgs <- edx %>% 
+  group_by(movieId) %>% 
+  summarize(b_i2 = sum(rating - mu_hat)/(n()+lambda), n_i = n()) 
+
+predicted_ratings <- validation %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu_hat + b_u+ b_i2) %>%
+  .$pred
+
+model_3_rmse <- RMSE(predicted_ratings, validation$rating)
+#model_3_rmse #for result testing
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame("Predictive Method"="Regularized Movie + User Effects Model",  
+                                     RMSE = model_3_rmse ))
+
+#rmse_results %>% knitr::kable() #for result testing
+
+#RMSE of including average, regularized movie effect model and user effect model: 0.8652263
+
+##########################################################
+# 1e.Regularization for user-specifc effect in the model
+##########################################################
+
+#Choosing the penalty terms using cross-validation
+lambdas <- seq(0, 10, 0.25)
+
+rmses <- sapply(lambdas, function(l){
+  
+  mu_hat <- mean(edx$rating)
+  
+  b_i <- edx %>% 
+    group_by(movieId) %>%
+    summarize(b_i = sum(rating - mu_hat)/(n()+l))
+  
+  b_u <- edx %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>%
+    summarize(b_u = sum(rating - b_i - mu_hat)/(n()+l))
+  
+  predicted_ratings <- 
+    validation %>% 
+    left_join(b_i, by = "movieId") %>%
+    left_join(b_u, by = "userId") %>%
+    mutate(pred = mu_hat + b_i + b_u) %>%
+    pull(pred)
+  
+  return(RMSE(predicted_ratings, validation$rating))
+})
+
+
+#qplot(lambdas, rmses)  
+#lambdas[which.min(rmses)]
+#lambda = 5.25 based on the above cross-validation 
+
+lambda <- lambdas[which.min(rmses)]
+
+movie_reg_avgs <- edx %>% 
+  group_by(movieId) %>% 
+  summarize(b_i2 = sum(rating - mu_hat)/(n()+lambda), n_i = n()) 
+
+user_reg_avgs <- edx %>% 
+  left_join(movie_reg_avgs, by="movieId") %>%
+  group_by(userId) %>% 
+  summarize(b_u2 = sum(rating - mu_hat-b_i2)/(n()+lambda), n_i = n()) 
+
+predicted_ratings <- validation %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(user_reg_avgs, by='userId') %>%
+  mutate(pred = mu_hat + b_i2 + b_u2) %>%
+  .$pred
+
+model_4_rmse <- RMSE(predicted_ratings, validation$rating)
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame("Predictive Method"="Regularized Movie + Regularized User Effects Model",  
+                                     RMSE = model_4_rmse ))
+
+#rmse_results %>% knitr::kable() #for result testing
+
+#RMSE of including average, regularized movie effect model and regularized user effect model: 0.8648170 
 
 ##########################################################
 # 2.Final Result
 ##########################################################
 
-data_frame("Final Result" ="Movie + User Effects Model",RMSE = model_2_rmse )%>% knitr::kable()
+data_frame("Final Result" ="Regularized Movie + Regularized User Effects Model",RMSE = model_4_rmse )%>% knitr::kable()
 
-#Final RMSE (including average, movie effect model and user effect model): 0.8292477 
+#Final RMSE (including average, movie effect model and user effect model): 0.8648170 
